0731f90d825108eaec4cc09e415c0509b9663162
dimoninbirsk <dimoninbirsk@gmail.com>
2022-10-17 15:12:03 +0500

    Complete backend authorization

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,5 +1,50 @@
-class TokenService {
+const jwt = require('jsonwebtoken')
+const tokenModel = require('../models/token-model')
 
+class TokenService {
+    generateTokens(payload) {
+        const accesToken = jwt.sign(payload, process.env.JWT_ACCES_SECRET, {expiresIn: '30m'})
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
+        return {
+            accesToken,
+            refreshToken
+        }
+    }
+
+    async saveToken(userId, refreshToken) {
+        const tokenData = await tokenModel.findOne({user: userId})
+        if(tokenData) {
+            tokenData.refreshToken = refreshToken;
+            return tokenData.save();
+        }
+        const token = tokenModel.create({user: userId, refreshToken})
+        return token;
+    }
+    async removeToken(refreshToken) {
+        const tokenData = await tokenModel.deleteOne({refreshToken})
+        return tokenData;
+    }
+
+    validateAccesToken(token) {
+        try {
+            const userData = jwt.verify(token,  process.env.JWT_ACCES_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+    validateRefreshToken(token) {
+        try {
+            const userData = jwt.verify(token,  process.env.JWT_REFRESH_SECRET);
+            return userData;
+        } catch (e) {
+            return null;
+        }
+    }
+    async findToken(refreshToken) {
+        const tokenData = await tokenModel.findOne({refreshToken})
+        return tokenData;
+    }
 }
 
 module.exports = new TokenService();
